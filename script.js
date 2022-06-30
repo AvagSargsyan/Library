fdff0579a25e0da49bb01fd5b4fa4c31ebfd187d
Avag <avagsargsyan01@gmail.com>
2022-06-30 16:59:58 +0400

    Add final features add changable status state button, remove and remove all buttons

--- script.js
+++ script.js
@@ -4,16 +4,10 @@ const formAuthor = document.querySelector('#author');
 const formPages = document.querySelector('#pages');
 const formRead = document.querySelector('#read');
 const addBookBtn = document.querySelector('.add-book');
+const removeAllBtn = document.querySelector('.remove-all');
+const titleLabel = document.querySelector('[for="title"]');
 
-let myLibrary = [
-    {
-        title: 'Book one',
-        author: 'Someone',
-        pages: '220',
-        read: false,
-        isDisplayed: false
-    }
-];
+let myLibrary = [];
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -32,7 +26,7 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function display(arr) {
-    arr.forEach(book => {
+    arr.forEach((book, i) => {
         if (book.isDisplayed === false) {
             const row = document.createElement('tr');
             const title = document.createElement('td');
@@ -40,50 +34,89 @@ function display(arr) {
             const pages = document.createElement('td');
             const status = document.createElement('td');
             const remove = document.createElement('td');
-    
+            
             const statusBtn = document.createElement('button');
-            if (book.read === true) {
+            if (book.read) {
                 statusBtn.textContent = 'Read';
+                statusBtn.style.backgroundColor = 'green';
             } else {
                 statusBtn.textContent = 'Not read';
+                statusBtn.style.backgroundColor = 'red';
             }
             statusBtn.classList.add('status-btn');
-    
+            
             const removeBtn = document.createElement('button');
             removeBtn.textContent = 'Remove';
             removeBtn.classList.add('remove-btn');
     
             remove.appendChild(removeBtn);
             status.appendChild(statusBtn);
-    
+            
             title.innerText = book.title;
             author.innerText = book.author;
             pages.innerText = book.pages;
-    
+            
             table.appendChild(row);
             table.appendChild(title);
             table.appendChild(author);
             table.appendChild(pages);
             table.appendChild(status);
             table.appendChild(remove);
-
+            
             book.isDisplayed = true;
 
-            // removeBtn.addEventListener('click', (e) => {
-            //     book.removable = true;
-            //     myLibrary.filter(bk => !(book.removable));
-            //     display(myLibrary);
-            // });
+            removeBtn.addEventListener('click', e => {
+                let newLib = myLibrary.filter(b => b !== book);
+                title.remove();
+                author.remove();
+                pages.remove();
+                status.remove();
+                remove.remove();
+
+                myLibrary = newLib;
+                display(myLibrary);
+                console.log(myLibrary);
+            });
+
+            statusBtn.addEventListener('click', e => {
+                book.read = !(book.read);
+                if (statusBtn.innerText === 'Read') {
+                    statusBtn.innerText = 'Not read';
+                    statusBtn.style.backgroundColor = 'red';
+                } else {
+                    statusBtn.innerText = 'Read';
+                    statusBtn.style.backgroundColor = 'green';
+                }
+            });
+
+            removeAllBtn.addEventListener('click', e => {
+                myLibrary = [];
+                display(myLibrary);
+                console.log('hi');
+
+                title.remove();
+                author.remove();
+                pages.remove();
+                status.remove();
+                remove.remove();
+            });
         }
     });
 }
 
 addBookBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    addBookToLibrary(formTitle.value, formAuthor.value, formPages.value, formRead.checked);
-    display(myLibrary);
+    if (formTitle.value !== '') {
+        addBookToLibrary(formTitle.value, formAuthor.value, formPages.value, formRead.checked);
+        display(myLibrary);
+        formTitle.value = '';
+        formAuthor.value = '';
+        formPages.value = '';
+        formRead.checked = false;
+        titleLabel.style.color = 'rgb(37, 37, 37)';
+    } else {
+        titleLabel.style.color = 'red';
+    }
 });
 
 display(myLibrary);
-
-console.log(myLibrary);
